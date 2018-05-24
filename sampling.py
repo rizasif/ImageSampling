443aefd23cd7ae88b5e8426ed76d478eada1e490
Rizwan Asif <rizasif@gmail.com>
2018-05-24 11:52:04 +0500

    added timestamp

--- sampling.py
+++ sampling.py
@@ -12,8 +12,9 @@ def sample_file(filename, start_image_name):
   success = True
   while success:
     if skip % sampling_rate == 0:
+      timestamp = vidcap.get(cv2.cv.CV_CAP_PROP_POS_MSEC)
       cv2.imwrite("Images/frame%d.jpg" % count, image)     # save frame as JPEG file
-      print 'Saving frame: ', count
+      print "Saving frame: {} at {}ms".format(count,timestamp) 
       count += 1
     success,image = vidcap.read()
     skip += 1
